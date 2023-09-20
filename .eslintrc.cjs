a91ab7631926548ccf93393060de017060734aaf
titus <tjb@symcon.de>
2023-09-20 21:38:39 +0200

    es lint and yarn config and lock

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "eol-last": 0,
+        "no-multiple-empty-lines": 0,
+        "@typescript-eslint/strict-boolean-expressions": 0
+    }
+}
